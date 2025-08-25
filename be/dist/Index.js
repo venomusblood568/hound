e4b037d43e5341dc554b0ad15d2e00ca1b792701
GOURAV_ANAND_JHA <90522738+venomusblood568@users.noreply.github.com>
2025-08-25 08:46:09 +0530

    backend init

---
+++ be/dist/Index.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const cors_1 = __importDefault(require("cors"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const PORT = process.env.PORT || 3001;
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+// Tester backend
+app.get("/", (req, res) => {
+    res.status(200).json({ Message: "Backend is Running...." });
+});
+app.listen(PORT, () => {
+    console.log(`Server Running at http://localhost:${PORT}`);
+});
---
+++ be/src/Index.ts
@@ -0,0 +1,20 @@
+import express, { Request,Response } from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 3001;
+
+app.use(cors())
+app.use(express.json())
+
+// Tester backend
+app.get("/",(req:Request,res:Response) =>{
+    res.status(200).json({Message:"Backend is Running...."})
+})
+
+app.listen(PORT, () => {
+    console.log(`Server Running at http://localhost:${PORT}`)
+})
